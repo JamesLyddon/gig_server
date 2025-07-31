ea5c6d6325507fb24486b19d62184ff0cc1dc4c8
james lyddon <jameslyddon@gmail.com>
2025-07-31 16:54:41 +0100

    TDD /gigs/:id endpoint and created indexFromId middleware

--- app.js
+++ app.js
@@ -4,11 +4,35 @@ const gigs = require('./data/gigsData.js')
 
 const app = express();
 app.use(express.json())
-
 app.use(cors());
+
+// middleware
+const indexFromId = (req, res, next) => {
+  const { id } = req.params
+  const gigId = parseInt(id)
+
+  if(isNaN(gigId)) return res.sendStatus(400)
+  
+  const gigIndex = gigs.findIndex(
+    gig => gig.id === gigId
+  )
+
+  if(gigIndex === -1) return res.sendStatus(404)
+
+  req.gigIndex = gigIndex
+  next()
+}
+
 
 app.get('/gigs', (req, res) => {
   res.json(gigs)
 })
+
+// /gigs/:id	GET	{"gig": {Gig where the id matches the one provided in the url}}
+app.get('/gigs/:id', indexFromId, (req, res) => {
+  const { gigIndex } = req
+  res.json(gigs[gigIndex])
+})
+
 
 module.exports = app
--- app.test.js
+++ app.test.js
@@ -11,3 +11,40 @@ describe('GET /gigs', () => {
         expect(response.status).toBe(200);
     })
 });
+
+describe('GET /gigs/:id', () => {
+    test('returns Sigur Rós gig when id is 1', async () => {
+        const response = await request(app).get('/gigs/1')
+        const formattedResponse = {...response.body, date: new Date(response.body.date)}
+
+        expect(formattedResponse).toEqual(gigs[0])
+        expect(response.status).toBe(200)
+    })
+    test('returns Chumbawamba gig when id is 3', async () => {
+        const response = await request(app).get('/gigs/3')
+        const formattedResponse = {...response.body, date: new Date(response.body.date)}
+
+        expect(formattedResponse).toEqual(gigs[2])
+        expect(response.status).toBe(200)
+    })
+    test('returns Chumbawamba gig when id is 3', async () => {
+        const response = await request(app).get('/gigs/3')
+        const formattedResponse = {...response.body, date: new Date(response.body.date)}
+
+        expect(formattedResponse).toEqual(gigs[2])
+        expect(response.status).toBe(200)
+    })
+    test('returns Bad Request 400 error when id is "a"', async () => {
+        const response = await request(app).get('/gigs/a')
+
+        expect(response.text).toEqual("Bad Request")
+        expect(response.status).toBe(400)
+    })
+    test('returns Not Found 404 error when id is 5', async () => {
+        const response = await request(app).get('/gigs/5')
+
+        expect(response.text).toEqual("Not Found")
+        expect(response.status).toBe(404)
+    })
+})
+
